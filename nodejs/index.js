4d630b03858418a422b72c3a86a35d61192d2350
Palash Choudhury <palashc@contata.co.in>
2019-04-09 14:13:45 +0530

    got image list in nodejs

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,6 @@
 const Webdriver = require('selenium-webdriver');
+const Until = Webdriver.until;
+const By = Webdriver.By;
 const chromedriver = require('chromedriver');
 let chrome = require('selenium-webdriver/chrome');
 // const firefox = require('selenium-webdriver/firefox');
@@ -10,15 +12,16 @@ class InstagramImageDownloader{
             self.url = `https://www.instagram.com/${username}/`
         } else {
             self.url = url
-        } 
+        }
     }
 
     setup(){
         this.setup_chrome()
+        this.driver.get(this.url)
     }
-    
+
     setup_chrome(self){
-        let chromeCapabilities = webdriver.Capabilities.chrome();
+        let chromeCapabilities = Webdriver.Capabilities.chrome();
         // Setting browser to be incognito and headless
         let chromeOptions = {
             'args': ['--headless', '--incognito']
@@ -31,45 +34,73 @@ class InstagramImageDownloader{
             .withCapabilities(chromeCapabilities)
             .build();
     }
-        
-    setup_firefox(self){
-        // TODO: 
-        // firefox_options = Options()  
-        // firefox_options.add_argument("--headless")  
-        // firefox_options.binary_location = FIREFOX_BINARY_LOCATION    
 
-        // self.driver = webdriver.Firefox(executable_path=GECKO_DRIVER_LOCATION, options=firefox_options)  
+    setup_firefox(self){
+        // TODO:
+        // firefox_options = Options()
+        // firefox_options.add_argument("--headless")
+        // firefox_options.binary_location = FIREFOX_BINARY_LOCATION
+
+        // self.driver = webdriver.Firefox(executable_path=GECKO_DRIVER_LOCATION, options=firefox_options)
         // self.driver.get(self.url)
         this.driver = new webdriver.Builder()
             .forBrowser('firefox')
             .setFirefoxOptions(/* ... */)
-            .build(); 
+            .build();
     }
 
-    get_image_list(self, depth=10){
+    async get_image_list(depth=10){
         let image_list = {}
+        let waitForIt = false
         let self = this
         try{
             self.setup()
-            // # Load whole page
-            self.load_more(depth)
-            // # The image tags we need to worry about
-            let img_elements = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.TAG_NAME, "article"))
-            )
-            img_elements = img_elements.find_elements_by_tag_name('a')
-            // # image_list = [img.get_attribute('src') for img in img_elements]
-            img_elements.forEach((img, key)=>{
-                const img_img = img.find_elements_by_tag_name('img')
-                if(len(img_img) > 0){
-                    const link = img.get_attribute('href')
-                    const src = img_img[0].get_attribute('src')
-                    image_list[link] = src
+            async function fetcher() {
+                return new Promise((resolve, reject)=>{
+                    // # Load whole page
+                    // self.load_more(depth)
+                    // # The image tags we need to worry about
+                    let img_elements = self.driver.wait(Until.elementLocated(By.css("article")), 10000)
+                    img_elements.then((img_elements)=>{
+                        img_elements = img_elements.findElements(By.css('a'))
+                        img_elements.then((img_elements)=>{
+                            // # image_list = [img.get_attribute('src') for img in img_elements]
+                            const image_count = img_elements.length;
+                            img_elements.forEach((img, key)=>{
+                                img.findElements(By.css('img')).then(async (img_img)=>{
+                                    if(img_img.length > 0){
+                                        const link = await img.getAttribute('href')
+                                        const src = await img_img[0].getAttribute('src')
+                                        image_list[link] = src
+                                    }
+                                    if(image_count == Object.keys(image_list)){
+                                        resolve(image_list)
+                                    }
+                                })
+                            })
+                        }).catch(err=>reject)
+                    }).catch(err=>reject)
+                })
+            }
+            image_list = await fetcher()
+            console.log(image_list)
+            /* waitForIt = Object.keys(image_list).length != image_count
+            const intervalTimer = setInterval(()=>{
+                if(Object.keys(image_list).length == image_count){
+                    clearInterval(intervalTimer);
+                    self.driver.quit()
                 }
-            })
+            }, 500); */
+        } catch(err){
+            console.log(err)
         } finally{
             self.driver.quit()
         }
         return image_list
     }
 }
+
+let iid = new InstagramImageDownloader('a_geologist')
+iid.get_image_list(10).then((data)=>{
+    console.log(data)
+})
